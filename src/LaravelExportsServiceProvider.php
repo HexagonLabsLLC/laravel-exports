b5db1c86af45b59bc62ae03bbc7f7e8e7e136547
Robert <robert.klitsch97@gmail.com>
2025-06-11 00:09:44 -0700

    Add LaravelExportsServiceProvider for service registration and configuration publishing

---
+++ src/LaravelExportsServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Hexlabs\LaravelExports;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelExportsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/laravel-exports.php', 'laravel-exports');
+
+        // Register the main service
+        $this->app->singleton('laravel-exports', function ($app) {
+            return new Services\DynamicExportService;
+        });
+    }
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/laravel-exports.php' => config_path('laravel-exports.php'),
+        ], 'config');
+
+        $this->publishes([
+            __DIR__.'/../database/migrations/' => database_path('migrations'),
+        ], 'migrations');
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                Console\Commands\ImportModelsCommand::class,
+                Console\Commands\SeedTransformationFunctionsCommand::class,
+                Console\Commands\TestDatabaseCommand::class,
+            ]);
+        }
+    }
+}
